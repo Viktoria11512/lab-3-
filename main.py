8370c0a297ad68bdb727d046b10f8ce41834105a
yevaaaaaaa <yevabaryshevska@knu.ua>
2024-02-13 21:59:56 +0200

    fixed problems

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ print(number,factorial(number))
 
 n = int(input())
 print(res(n))
+
+a = int(input())
+b = int(input())
+print(zm(a, b))
--- utils.py
+++ utils.py
@@ -4,21 +4,14 @@ def factorial(n):
     else:
         return n*factorial(n-1)
 
-
 def res(n):
-
-
- chs = 1
-for i in range(1, n + 1):
- chs = chs * i // zm(chs, i)
-return chs
-
+    chs = 1
+    for i in range(1, n + 1):
+        chs = chs * i // zm(chs, i)
+    return chs
 
 def zm(a, b):
- while b != 0:
-  a, b = b, a % b
+    while b != 0:
+        a, b = b, a % b
 
-
-return a
-n = int(input())
-print(res(n))
+    return a
