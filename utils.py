97b17126e7c43479fd39f52e74dfa95bd29ac9dc
Viktoria11512 <viktoriadavigora@knu.ua>
2024-02-13 21:54:09 +0200

    Виправлення помилок

--- utils.py
+++ utils.py
@@ -3,3 +3,22 @@ def factorial(n):
         return 1
     else:
         return n*factorial(n-1)
+
+
+def res(n):
+
+
+ chs = 1
+for i in range(1, n + 1):
+ chs = chs * i // zm(chs, i)
+return chs
+
+
+def zm(a, b):
+ while b != 0:
+  a, b = b, a % b
+
+
+return a
+n = int(input())
+print(res(n))
